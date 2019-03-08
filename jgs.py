c79ba2e37673ce903a44294af236933b7d248a2c
JeongGwan Lee <leejk526@gmail.com>
2019-03-08 18:42:11 +0900

    g

--- jgs.py
+++ jgs.py
@@ -69,7 +69,45 @@ target_token_index = dict(
         [(char, i) for i, char in enumerate(target_characters)])
 
 encoder_input_data = np.zeros(
-        (len(input_texts
+        (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
+        dtype='float32')
+decoder_input_data = np.zeros(
+        (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+        dtype='float32')
+decoder_target_data = np.zeros(
+        (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+        dtype='float32')
+
+for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+    for t, char in enumerate(input_text):
+        ipdb.set_trace()
+        encoder_input_data[i, t, input_token_index[char]] = 1.
+    for t, char in enumerate(target_text):
+        # decoder_target_data is ahead of decoder_input_data by one timestep
+        ipdb.set_trace()
+        decoder_input_data[i, t, target_token_index[char]] = 1.
+        if t > 0:
+            #
+            #
+            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+
+
+# Define an input sequence and process it.
+encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder = LSTM(latent_dim, return_state=True)
+encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+# We discard 'encoder_outputs' and only keep the states.
+encoder_states = [state_h, state_c]
+
+# Set up the decoder, using ''
+decoder_inputs = Input(shape=(None, num_decoder_tokens))
+#
+#
+#
+decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+                                     initial_state=encoder_states)
+decoder_dense = Dense(num_decoder_tokens, activation='softmax')
 
 ipdb.set_trace()
 
